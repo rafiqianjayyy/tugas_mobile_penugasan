09d515172130b5b2af2716b47fb4c27ee8ebd266
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-07 11:44:25 +0700

    Create increment.dart

---
+++ increment.dart
@@ -0,0 +1,11 @@
+//INI ADALAH INCREMENT DAN DECREMENT
+void main(){
+  var i = 0;
+  i++;
+
+  var j = i++; //i nya dulu baru di tambah 1 jadi j itu = i dan i itu sama dengan 0
+   var K = ++i; // i nya di tambah dulu baru di tampilkan
+   print(i);
+   print(j);
+   print(K);
+ }
