fda8a7fae7cbff99d1508f175c9f91af8cc7e63d
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-07 11:42:41 +0700

    Create penugasan.dart

---
+++ penugasan.dart
@@ -0,0 +1,15 @@
+//INI ADALAH OPERATOR PENUGASAN (karena banyak jadi ulun contohkan 2 aja)
+ void main(){
+   var a = 10;
+
+   a= a + 15;
+
+   a += 5;
+
+  var b = 10;
+
+  b -= 20;
+
+  print(a);
+  print(b);
+ }
